021de12bcaa828ab6952abcf3e6e195552574b12
orchlonn <60744465+orchlonn@users.noreply.github.com>
2022-08-31 16:43:06 +0800

    initial commit

---
+++ pages/app-ads.txt/index.js
@@ -0,0 +1,10 @@
+import Head from "next/head";
+import Link from "next/link";
+
+export default function Linking() {
+  return (
+    <div>
+      <p> facebook.com, 1097349567558202, RESELLER, c3e20eee3f780d68</p>
+    </div>
+  );
+}
